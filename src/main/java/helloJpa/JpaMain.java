6c44a505e9d53dadea54929e322d96d2515ecb8c
tacotata <71251163+tacotata@users.noreply.github.com>
2022-05-14 23:56:01 +0900

    영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해 도 실제 엔티티 반환 -참을 반환

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -1,7 +1,5 @@
 package helloJpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.*;
 import java.sql.SQLOutput;
 import java.time.LocalDateTime;
@@ -30,25 +28,37 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            //refMember = class helloJpa.Member$HibernateProxy$umY121eh
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass());
+            Member m1 = em.find(Member.class, member1.getId());
+            System.out.println("m1.getClass() = " + m1.getClass());
 
-            //준영속성 상태
-            //org.hibernate.proxy.AbstractLazyInitializer.initialize
-            // 아래의 이런걸 만난순간 could not initialize proxy [helloJpa.Member#1] - no Session
-            em.detach(refMember);
-            //em.close();
-            //em.clear();
-            System.out.println("refMember.getUsername() = " + refMember.getUsername());
+            //m1.getClass() = class helloJpa.Member
+            //reference.getClass() = class helloJpa.Member
+            Member reference = em.getReference(Member.class, member1.getId());
+            //1. 1차 캐시에 있는데 굳이 프록시로 가져올 필요 있냐 성능최적화입장에서 원본가져오는게 나음
+            //2. == 비교를 true로 만들어주기 위해서?
+            System.out.println("reference.getClass() = " + reference.getClass());
+            //이건 항상 true가 되야함 한 영속성안에서 pk가 같으면 true를 반환해줘야함 기본 매커니즘
+            //한 트랙지션에서  같은걸 보장해줌 프록시를 가져오더라도 타입이 안맞으면 false로 나오니까
+            //== 비교할 때 true를 만들어주기위해서 영속성 컨텍스트에 있으면 실제를 반환 true를 보장해줌
+            System.out.println("a == a : "+ (m1 ==reference));
 
-            //refMember.getUsername();
+//
+//            //refMember = class helloJpa.Member$HibernateProxy$umY121eh
+//            Member refMember = em.getReference(Member.class, member1.getId());
+//            System.out.println("refMember = " + refMember.getClass());
+//
+//            //findMember = class helloJpa.Member$HibernateProxy$umY121eh
+//            Member findMember = em.find(Member.class, member1.getId());
+//            System.out.println("findMember = " + findMember.getClass());
+//
+//            // 참을 반환 하기 위해서  find도 프록시를 가져옴
+//            System.out.println("refMember == findMember : "+ (refMember ==findMember));
+
 
 
             tx.commit();
         }catch (Exception e){
             tx.rollback();
-            e.printStackTrace();
         }finally{
             //엔티티 매니저 꼭 닫아줘야함
             em.close();
@@ -58,3 +68,7 @@ public class JpaMain {
 
 
 }
+
+
+
+
