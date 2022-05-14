ed398e0db2a0b04cce1e9385ab48f6d5d242a43f
tacotata <71251163+tacotata@users.noreply.github.com>
2022-05-14 23:57:33 +0900

    프록시 객체는 원본 엔티티를 상속받음, 따라서 타입 체크시 주의 instanceof 사용

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -20,41 +20,21 @@ public class JpaMain {
 
             Member  member1 = new Member();
             member1.setUsername(("member1"));
+            Member  member2 = new Member();
+            member2.setUsername(("member2"));
 
             em.persist(member1);
-
+            em.persist(member2);
 
             //영속성 컨텍스트 깔끔
             em.flush();
             em.clear();
 
             Member m1 = em.find(Member.class, member1.getId());
-            System.out.println("m1.getClass() = " + m1.getClass());
+            Member m2 = em.getReference(Member.class, member2.getId());
 
-            //m1.getClass() = class helloJpa.Member
-            //reference.getClass() = class helloJpa.Member
-            Member reference = em.getReference(Member.class, member1.getId());
-            //1. 1차 캐시에 있는데 굳이 프록시로 가져올 필요 있냐 성능최적화입장에서 원본가져오는게 나음
-            //2. == 비교를 true로 만들어주기 위해서?
-            System.out.println("reference.getClass() = " + reference.getClass());
-            //이건 항상 true가 되야함 한 영속성안에서 pk가 같으면 true를 반환해줘야함 기본 매커니즘
-            //한 트랙지션에서  같은걸 보장해줌 프록시를 가져오더라도 타입이 안맞으면 false로 나오니까
-            //== 비교할 때 true를 만들어주기위해서 영속성 컨텍스트에 있으면 실제를 반환 true를 보장해줌
-            System.out.println("a == a : "+ (m1 ==reference));
-
-//
-//            //refMember = class helloJpa.Member$HibernateProxy$umY121eh
-//            Member refMember = em.getReference(Member.class, member1.getId());
-//            System.out.println("refMember = " + refMember.getClass());
-//
-//            //findMember = class helloJpa.Member$HibernateProxy$umY121eh
-//            Member findMember = em.find(Member.class, member1.getId());
-//            System.out.println("findMember = " + findMember.getClass());
-//
-//            // 참을 반환 하기 위해서  find도 프록시를 가져옴
-//            System.out.println("refMember == findMember : "+ (refMember ==findMember));
-
-
+           // true
+            System.out.println(" m1== m2" +(m1 instanceof Member));
 
             tx.commit();
         }catch (Exception e){
