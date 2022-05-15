8a61661df9ab74e818b144771c9b76f48bb72bd6
tacotata <71251163+tacotata@users.noreply.github.com>
2022-05-15 18:50:48 +0900

    프로젝션 여러값 조회

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -22,34 +22,32 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+            //프로젝션 - 여러 값 조회
+            /*1.Query  - 타입을 명시 못하니까 오브젝트로 돌려주는거야
+            List resultList =  em.createQuery("select m.username, m.age from Member m")
+                            .getResultList();
+            Object o = resultList.get(0);
+            Object[] result = (Object[]) o;
+            System.out.println("result[0] = " + result[0]);
+            System.out.println("result[0] = " + result[1]);*/
 
-            //보통 타입쿼리로 하지않고 체이닝으로 엮음 메서드 체이닝을 활용하도로 설계가 되어있음
-            Member result = em.createQuery("select m from Member m where m.username =:username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("singleResult = " + result.getUsername());
+            /*2.제네릭에 선언 Object[] 타입으로 조회
+            List<Object[]>  resultList =  em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
 
+            Object[] result = resultList.get(0);
+            System.out.println("result[0] = " + result[0]);
+            System.out.println("result[0] = " + result[1]);*/
 
-            //타입 정보 명확함 결과가 하나 이상일 때getResultList
-            //결과 없으면 빈 리스트 반환
-//            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-//            List<Member> resultList = query1.getResultList();
-//            for (Member member1 : resultList) {
-//                System.out.println("member1 = " + member1);
-//            }
-            //결과가 정확히 하나getSingleResult
-            //결과가 없으면: javax.persistence.NoResultException
-            //둘 이상이면: javax.persistence.NonUniqueResultException
-//            TypedQuery<Member> query = em.createQuery("select m from Member m where m.id =10", Member.class);
-//            Member result = query.getSingleResult();
-//            System.out.println("result = " + result);
+            //3.제일 깔끔 new 명령어로 조회
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO( m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
 
-            //타입 정보 명확함
-            //TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-
-            //username , age 타입정보 다름 반환 타입 명확하지 않음
-           //Query query3 = em.createQuery("select m.username, m.age from Member m");
-
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
 
             tx.commit();
