ee7564b4a49b6d8418874c59a6573dac8db5eab2
tacotata <71251163+tacotata@users.noreply.github.com>
2022-05-15 17:14:23 +0900

    JPA는 다양한 쿼리 방법

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -1,6 +1,9 @@
 package helloJpa;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,47 +21,46 @@ public class JpaMain {
         tx.begin();
 
         try{
+
+            //jpql
+//            List<Member> result = em.createQuery(
+//                    "select  m From Member m where m.username like '%kim%'",
+//                    Member.class).getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member);
+//            }
+
+            //Criteria 사용 준비
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+//
+//            Root<Member> m = query.from(Member.class);
+//            //장점  1.컴파일 오류 2.동적쿼리
+//            //단점 1.sql스럽지 않다. 실무에서 안써요 유지보수가 안되서
+//            //그래서 QueryDSL  사용하세요
+//            CriteriaQuery<Member> cq = query.select(m);
+//            String  username ="sss";
+//            if(username != null){
+//               cq  = cq.where(cb.equal(m.get("username"), "kim"));
+//            }
+//
+//            List<Member> resultList = em.createQuery(cq).getResultList();
+
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "zipcode"));
-
-            member.getFavoriteFoods().add("타코");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressesHistory().add(new AddressEntity("old1", "street", "zipcode"));
-            member.getAddressesHistory().add(new AddressEntity("old2", "street", "zipcode"));
-
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            //flush 언제 호출되냐면 -> 1.commit될때도 , 2.query가 날라갈때도
+            //결과가 0 됨 이럴때는 강제로 em.flush()해줘야함
+            //dbconn.excuteQuery("select * from member"); ->이건 jpa랑 관련없어서
+            //네이티브 SQL
+            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city , street , zipcode, USERNAME from MEMBER", Member.class)
+                            .getResultList();
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
-            System.out.println("==========START===============");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity  이 코드는 안됨 값타입은 불변해야함
-            //findMember.getHomeAddress().setCity("newCity");
-
-            //새로 넣어야함  address 인스턴스 자체를 갈아끼워야함
-            //값타입 안에 필드 하나만 바꾼다 아예 교체를 해줘야함
-           // Address a = findMember.getHomeAddress();
-            //findMember.setHomeAddress(new Address("newCity",a.getStreet(), a.getZipcode()));
-
-
-            //값타입 컬렉션 업데이트
-            //타코 -> 한식
-            //findMember.getFavoriteFoods().remove("타코");
-            //findMember.getFavoriteFoods().add("한식");
-
-            //멤버에 의존관계를 다 맡김 멤버만 라이프사이클 관리되는거임
-
-
-            //equls로 됨 그래서 hashcode 제대로 해야함
-            //기대한대로 값은 들어갔으나 address통으로 삭제하고 insert 2번함 왜?
-            //전체 삭제하고 남아있는 데이터 다시 insert함
-            // findMember.getAddressesHistory().remove(new Address("old1", "street", "zipcode"));
-            // findMember.getAddressesHistory().remove(new Address("newCity1", "street", "zipcode"));
 
             tx.commit();
         }catch (Exception e){
