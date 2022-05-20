1a2da7ff0786b0ac999258c172753d31804a07bf
tacotata <71251163+tacotata@users.noreply.github.com>
2022-05-20 19:36:22 +0900

    엔티티 직접 사용

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -44,33 +44,22 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            //페치조인은 이렇게 별칭을 주면 안됨 join fetch를 몇단계로 가져가서 사용할 때
-            // 둘 이상의 컬렉션은 페치 조인 할 수 없다.
-            // 컬렉션을 페치 조인하면 페이징 API(setFirstResult,setMaxResults)를 사용할 수 없다.
-            //
-            //일대일, 다대일 같은 단일 값 연관 필드들은 페치 조인해도 페이징 가능
-           // String query  = "select  t From Team t  join fetch t.members m ";
+            //===JPQL에서 엔티티를 직접 사용하면 SQL에서 해당 엔티티의 기본 키 값을 사용
+            //member0_.id=?
+                //String query  = "select  m From Member m where m = :member ";
+            // member0_.id=?
+                //String query  = "select  m From Member m where m.id = :memberId ";
+            //===엔티티 직접 사용 - 외래 키 값 ->  @JoinColumn(name ="TEAM_ID") 이거 말함
+            //  member0_.TEAM_ID=?
+            String query  = "select  m From Member m where m.team = :team ";
 
-            String query  = "select  t From Team t ";
 
-            List<Team> result =  em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
-                    .getResultList();
-            System.out.println("result = " + result.size());
+             List<Member> members  = em.createQuery(query, Member.class)
+                             .setParameter("team", teamA)
+                             .getResultList();
 
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() + "|" + team.getMembers().size());
-                for(Member member : team.getMembers()){
-                    //팀A에 멤버가 두명이라서 두번 나옴 똑같은 값이
-                    System.out.println("member = " + member);
-                }
-                //String query  = "select  m From Member m  "; 쿼리 4번
-                //회원1, 팀A(SQL)
-                //회원2, 팀A(1차캐시)
-                //회원3, 팀B(SQL)
-
-                //회원 100명 -> N + 1
+            for (Member member : members) {
+                System.out.println("member = " + member);
             }
 
             tx.commit();
